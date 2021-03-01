15c97cdffb395db8da04359212e39108f14bd0ea
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-03-01 13:55:04 +0000

    Add paging to JS

--- app.js
+++ app.js
@@ -1,17 +1,81 @@
 // DOM objects
 const mainScreen = document.querySelector('.main-screen');
 const mountName = document.querySelector('.mount-name');
-const mountId = document.querySelector('.mount-id');
+const mountIdNumber = document.querySelector('.mount-id');
 const mountFrontImage = document.querySelector('.mount-front-image');
 const mountWeight = document.querySelector('.mount-weight');
 const mountHeight = document.querySelector('.mount-height');
 const MountDescription = document.querySelector('.mount-description');
 const tooltip = document.querySelector('.tooltip');
 const mountListItems = document.querySelectorAll('.list-item');
-console.log(mountListItems);
+const leftButton = document.querySelector('.left-button')
+const rightButton = document.querySelector('.right-button')
 
 
-fetch('https://ffxivcollect.com/api/mounts/1')
+// variables 
+let allMountIds = [];
+let currentPage = 0;
+let totalPages = 0;
+
+
+// Paging
+const page = newPageNumber => {
+  if(newPageNumber < 0 || newPageNumber >= totalPages) {
+    return;
+  }
+  
+  currentPage = newPageNumber;
+  fetchMountData(allMountIds[newPageNumber]);
+};
+
+// Click events
+const handleLeftButtonClick = (e) => {
+  page(currentPage - 1);
+};
+const handleRightButtonClick = (e) => {
+  page(currentPage + 1);
+};
+
+const handleListItemClick = (e) => {
+    if (!e.target) return;
+
+    const listItem = e.target;
+    if (!listItem.textContent) return;
+
+    const mountId = listItem.textContent.split('.')[0];
+    fetchMountData(mountId);
+};
+
+// Get data for right side of screen
+fetch('https://ffxivcollect.com/api/mounts')
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        const { results } = data;
+        results.reverse();
+
+        allMountIds = results.map(results => results.id);
+
+        for (let i = 0; i < mountListItems.length ; i++) {
+            const mountListItem = mountListItems[i];
+            const resultData = results[i];
+
+            if (resultData) {
+                const { name, id} = resultData;                
+                mountListItem.textContent = id + '. ' + name;
+            } else {
+                mountListItem.textContent = '';
+            }
+        }
+  
+        // update the current mount id and paging info
+        totalPages = allMountIds.length;
+    });
+
+
+const fetchMountData = mountId => {
+    fetch(`https://ffxivcollect.com/api/mounts/${mountId}`)
     .then(res => {
         return   res.json();
     })
@@ -19,7 +83,7 @@ fetch('https://ffxivcollect.com/api/mounts/1')
         console.log(data);
         mainScreen.classList.remove('hide');
         mountName.textContent = data['name'];
-        mountId.textContent = '#' + data['id'].toString().padStart(3,0,0);
+        mountIdNumber.textContent = '#' + data['id'].toString().padStart(3,0,0);
         mountWeight.textContent = data['movement'];
         mountHeight.textContent = data['seats'];
         MountDescription.textContent = data['description'];
@@ -29,33 +93,14 @@ fetch('https://ffxivcollect.com/api/mounts/1')
         mountFrontImage.src = data['image'];
 
     });
+}
 
 
 
-// Get data for right side of screen
+// Event listeners
+rightButton.addEventListener('click', handleRightButtonClick); 
+leftButton.addEventListener('click', handleLeftButtonClick); 
 
-
-fetch('https://ffxivcollect.com/api/mounts')
-    .then(res => {
-        return   res.json();
-    })
-    .then(data => {
-        console.log(data);
-        const { results } = data;
-
-        console.log(results)
-
-        for (let i = 0; i < mountListItems.length ; i++) {
-            const mountListItem = mountListItems[i];
-            const resultData = results[i];
-
-
-            if (resultData) {
-                const { name } = resultData;
-                mountListItem.textContent = (name);
-            } else {
-                mountListItem.textContent = '';
-            }
-        }
-
-    });
+for (const mountListItem of mountListItems) {
+    mountListItem.addEventListener('click', handleListItemClick);
+}
