206866134605d388508fe0e423dc51421f902bc8
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-03-08 15:21:57 +0000

    Add Map JS

---
+++ map.js
@@ -0,0 +1,14 @@
+function myMap() {
+
+}
+
+const mapProp = {
+
+center: new google.maps.LatLng(51.515362938079186, -0.12731627286117514),
+
+zoom: 5,
+    };
+
+const map = new google.maps.Map(document.getElementById("map"), mapProp);   
+}
+
