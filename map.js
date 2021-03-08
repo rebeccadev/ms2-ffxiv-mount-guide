3b3e030a16db79878870b6e581303b0aa0cb3626
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-03-08 23:15:05 +0000

    Update Map JS file with lat and lng figures

--- map.js
+++ map.js
@@ -1,14 +1,15 @@
-function myMap() {
-
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const flagship = { lat: 51.515, lng: -0.127 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center
+  });
+  // The marker, positioned at flagship
+  const marker = new google.maps.Marker({
+    position: flagship,
+    map: map,
+  });
 }
-
-const mapProp = {
-
-center: new google.maps.LatLng(51.515362938079186, -0.12731627286117514),
-
-zoom: 5,
-    };
-
-const map = new google.maps.Map(document.getElementById("map"), mapProp);   
-}
-
